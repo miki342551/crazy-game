a7fc164c3731b6d970c5b37d9ec381a573fd40b9
miki342551 <shewamiki6@gmail.com>
2025-11-28 12:40:49 +0300

    Add Netlify support to base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? "/" : "/crazy-game/",
+  base: (process.env.VERCEL || process.env.NETLIFY) ? "/" : "/crazy-game/",
   server: {
     host: true,
     allowedHosts: true
